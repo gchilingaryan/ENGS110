fbe16770bd3afaeb687da33e921c911fb7245503
gchilingaryan <garikchilingaryan@yahoo.com>
2018-04-06 17:08:08 +0400

    Adding students.

--- main_new.py
+++ main_new.py
@@ -20,23 +20,29 @@ def askForAssignmentMarks(grades, student_grades, name):
     current_grades = {name: {}}
 
     for key in grades:
-        if student_grades[name][key] > -1 :
-            student_answer = raw_input("Your grade for " + key + " is " + str(student_grades[name][key]) + ". Do you want to change it?")
-            if student_answer == "Yes" or student_answer == "yes":
-                student_new_grade = int(raw_input("Insert your new grade for " + key))
-                if (student_new_grade >= 0 and student_new_grade <= 100) or student_new_grade == -1:
-                    current_grades[name][key] = student_new_grade
+        if name in student_grades.keys():
+            if student_grades[name][key] > -1 :
+                student_answer = raw_input("Your grade for " + key + " is " + str(student_grades[name][key]) + ". Do you want to change it? ")
+                if student_answer == "Yes" or student_answer == "yes":
+                    student_new_grade = int(raw_input("What is your Current Grade for " + key + ". Please insert -1 if you don't have a grade yet "))
+                    student_new_grade = checkNumber(key, current_grades, student_new_grade, name)
                 else:
-                    print "Wrong input! Input number from 0 to 100"
-            else:
-                current_grades[name][key] = student_grades[name][key]
+                    current_grades[name][key] = student_grades[name][key]
         else:
-            student_current_grade = int(raw_input("What is your Current Grade for: " + key + " . Please insert -1 if you don't have a grade yet"))
-            if (student_current_grade >= 0 or student_current_grade <= 100) or student_current_grade == -1:
-                current_grades[name][key] = student_current_grade
-            else:
-                print "Wrong input! Input number from 0 to 100"
+            student_new_grade = int(raw_input("What is your Current Grade for " + key + ". Please insert -1 if you don't have a grade yet "))
+            student_new_grade = checkNumber(key, current_grades, student_new_grade, name)
     return current_grades
+
+def checkNumber(key, current_grades, student_new_grade, name):
+    while True:
+        if (student_new_grade < 0 or student_new_grade > 100) and student_new_grade != -1:
+            print "Wrong input! Input number from 0 to 100"
+            student_new_grade = int(raw_input("What is your Current Grade for " + key + ". Please insert -1 if you don't have a grade yet "))
+        else:
+            print student_new_grade
+            current_grades[name][key] = student_new_grade
+            break
+
 
 def saveGrades(student_grades, current_grades):
     student_grades[current_grades.keys()[0]] = current_grades[current_grades.keys()[0]]
@@ -49,15 +55,15 @@ def printCurrentGrade(grades, current_grades, name):
     curr_grade = 0
     for key in current_grades[name]:
         if current_grades[name][key] != -1:
-            calc_grade = int(current_grades[name][key]) * grades[key] / 100
+            calc_grade = float(current_grades[name][key]) * grades[key] / 100
             curr_grade = curr_grade + calc_grade
 
     return curr_grade
 
 def matrix(curr_grade, conv_matrix):
     for i in range(len(conv_matrix)):
-        if float(curr_grade) >= int(conv_matrix[i]['min']):
-            print float(curr_grade)
+        if curr_grade >= int(conv_matrix[i]['min']):
+            print curr_grade
             print conv_matrix[i]['mark']
             break
 
