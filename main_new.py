f3458782912f9075cb62e6f2b0898e697a2dfe67
gchilingaryan <garikchilingaryan@yahoo.com>
2018-04-09 12:48:28 +0400

    Adding id's of students.

--- main_new.py
+++ main_new.py
@@ -13,50 +13,52 @@ def loadStudentGrades():
         student_grades = json.load(data_file)
 
     name = raw_input("Please, input your name ")
+    id = raw_input("Please, input your id ")
 
-    return student_grades, name
+    return student_grades, name, id
 
-def askForAssignmentMarks(grades, student_grades, name):
-    current_grades = {name: {}}
+def askForAssignmentMarks(grades, student_grades, id):
+    current_grades = {id: {}}
 
     for key in grades:
-        if name in student_grades.keys():
-            if student_grades[name][key] > -1 :
-                student_answer = raw_input("Your grade for " + key + " is " + str(student_grades[name][key]) + ". Do you want to change it? ")
+        if id in student_grades.keys():
+            if student_grades[id][key] > -1 :
+                student_answer = raw_input("Your grade for " + key + " is " + str(student_grades[id][key]) + ". Do you want to change it? ")
                 if student_answer == "Yes" or student_answer == "yes":
                     student_new_grade = int(raw_input("What is your Current Grade for " + key + ". Please insert -1 if you don't have a grade yet "))
-                    student_new_grade = checkNumber(key, current_grades, student_new_grade, name)
+                    checkNumber(key, current_grades, student_new_grade, id)
                 else:
-                    current_grades[name][key] = student_grades[name][key]
+                    current_grades[id][key] = student_grades[id][key]
         else:
             student_new_grade = int(raw_input("What is your Current Grade for " + key + ". Please insert -1 if you don't have a grade yet "))
-            student_new_grade = checkNumber(key, current_grades, student_new_grade, name)
+            checkNumber(key, current_grades, student_new_grade, id)
     return current_grades
 
-def checkNumber(key, current_grades, student_new_grade, name):
+def checkNumber(key, current_grades, student_new_grade, id):
     while True:
         if (student_new_grade < 0 or student_new_grade > 100) and student_new_grade != -1:
             print "Wrong input! Input number from 0 to 100"
             student_new_grade = int(raw_input("What is your Current Grade for " + key + ". Please insert -1 if you don't have a grade yet "))
         else:
-            print student_new_grade
-            current_grades[name][key] = student_new_grade
+            current_grades[id][key] = student_new_grade
             break
 
 
-def saveGrades(student_grades, current_grades):
+def saveGrades(student_grades, current_grades, name, id):
     student_grades[current_grades.keys()[0]] = current_grades[current_grades.keys()[0]]
+    student_grades[id]['name'] = name
     print (json.dumps(student_grades))
     file = open("gc_grades.json", "w")
     file.write(json.dumps(student_grades))
     file.close()
 
-def printCurrentGrade(grades, current_grades, name):
+def printCurrentGrade(grades, current_grades, name, id):
     curr_grade = 0
-    for key in current_grades[name]:
-        if current_grades[name][key] != -1:
-            calc_grade = float(current_grades[name][key]) * grades[key] / 100
-            curr_grade = curr_grade + calc_grade
+    for key in current_grades[id]:
+        if current_grades[id][key] != -1:
+            if not current_grades[id][key] == name:
+                calc_grade = float(current_grades[id][key]) * grades[key] / 100
+                curr_grade = curr_grade + calc_grade
 
     return curr_grade
 
@@ -69,10 +71,10 @@ def matrix(curr_grade, conv_matrix):
 
 def main():
     grades, conv_matrix = loadSetupData()
-    student_grades, name = loadStudentGrades()
-    current_grades = askForAssignmentMarks(grades, student_grades, name)
-    saveGrades(student_grades, current_grades)
-    curr_grade = printCurrentGrade(grades, current_grades, name)
+    student_grades, name, id = loadStudentGrades()
+    current_grades = askForAssignmentMarks(grades, student_grades, id)
+    saveGrades(student_grades, current_grades, name, id)
+    curr_grade = printCurrentGrade(grades, current_grades, name, id)
     matrix(curr_grade, conv_matrix)
 
 main()
