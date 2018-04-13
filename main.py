a606478c1c645afe9b7003cdf3fa1f9e0f9771ab
gchilingaryan <garikchilingaryan@yahoo.com>
2018-04-13 21:43:19 +0400

    Adding window for teachers. Bug fixes

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import json
 import hashlib
 
+# Satenik password - 1111
+# All students passwords - 1234
+
+
 def loadSetupData():
     with open('gc_setup.json') as data_file:
         course = json.load(data_file)
@@ -26,50 +30,80 @@ def loadStudentGrades():
 
     return student_grades
 
-def askForUserInfo(student_grades):
+def loadTeachers():
+    try:
+        file = open('gc_teachers.json', 'r+')
+
+        if file.readlines() == []:
+            file.write("{}")
+            file.close()
+    except:
+        file = open('gc_teachers.json', 'w')
+        file.write("{}")
+        file.close()
+
+    with open('gc_teachers.json') as data_file:
+        teachers = json.load(data_file)
+
+    return teachers
+
+def askForUserInfo():
+    user = raw_input("How you want to login as a teacher or as a student? ")
     name = raw_input("Please, input your name ")
     id = raw_input("Please, input your id ")
     password = raw_input("Please, input your password ")
 
-    return name, id, password
+    return user, name, id, password
 
-def checkPassword(id, name, password, student_grades):
+def checkPassword(id, name, password, student_grades, teachers):
     m = hashlib.sha224(password).hexdigest()
-    try:
-        while True:
-            if m == student_grades[id]['user']['password']:
-                print "Welcome " + name
-                break
-            else:
-                password = raw_input("Try again! ")
-                m = hashlib.sha224(password).hexdigest()
-    except:
-        pass
+    if id in student_grades.keys():
+        try:
+            while True:
+                if m == student_grades[id]['user']['password']:
+                    print "Welcome " + name
+                    break
+                else:
+                    password = raw_input("Try again! ")
+                    m = hashlib.sha224(password).hexdigest()
+        except:
+            pass
+    else:
+        try:
+            while True:
+                if m == teachers[id]['user']['password']:
+                    print "Welcome " + name
+                    break
+                else:
+                    password = raw_input("Try again! ")
+                    m = hashlib.sha224(password).hexdigest()
+        except:
+            pass
 
     return m
 
-def askForAssignmentMarks(grades, student_grades, id):
-    current_grades = {id: {"user" : {}, "grades" : {}}}
-
-    for key in grades:
-        if id in student_grades.keys():
-            if student_grades[id]["grades"][key] > -1 :
-                student_input = raw_input("Your grade for " + key + " is " + str(student_grades[id]["grades"][key]) + ". Do you want to change it? ")
-                if student_input == "Yes" or student_input == "yes":
-                    student_input = raw_input("What is your Current Grade for " + key + ". Please insert -1 if you don't have a grade yet ")
+def askForAssignmentMarks(user, grades, student_grades, id):
+    if user == "Student" or user == "student":
+        current_grades = {id: {"user": {}, "grades": {}}}
+        for key in grades:
+            if id in student_grades.keys():
+                if student_grades[id]["grades"][key] > -1 :
+                    student_input = raw_input("Your grade for " + key + " is " + str(student_grades[id]["grades"][key]) + ". Do you want to change it? ")
+                    if student_input == "Yes" or student_input == "yes":
+                        student_input = raw_input("What is your Current Grade for " + key + ". Please insert -1 if you don't have a grade yet ")
+                        student_input = ValidInput(key, student_input)
+                        ValidNumber(key, current_grades, student_input, id)
+                    else:
+                        current_grades[id]["grades"][key] = student_grades[id]["grades"][key]
+                else:
+                    student_input = raw_input("You have no grade for " + key + ". Please insert a grade or -1 again if you don't have a grade yet ")
                     student_input = ValidInput(key, student_input)
                     ValidNumber(key, current_grades, student_input, id)
-                else:
-                    current_grades[id]["grades"][key] = student_grades[id]["grades"][key]
             else:
-                student_input = raw_input("You have no grade for " + key + ". Please insert a grade or -1 again if you don't have a grade yet ")
+                student_input = raw_input("What is your Current Grade for " + key + ". Please insert -1 if you don't have a grade yet ")
                 student_input = ValidInput(key, student_input)
                 ValidNumber(key, current_grades, student_input, id)
-        else:
-            student_input = raw_input("What is your Current Grade for " + key + ". Please insert -1 if you don't have a grade yet ")
-            student_input = ValidInput(key, student_input)
-            ValidNumber(key, current_grades, student_input, id)
-    return current_grades
+        return current_grades
 
 def ValidNumber(key, current_grades, student_input, id):
     while True:
@@ -92,19 +126,57 @@ def ValidInput(key, student_input):
 
     return student_input
 
-def saveGrades(student_grades, current_grades, name, id, m):
-    student_grades[current_grades.keys()[0]] = current_grades[current_grades.keys()[0]]
-    student_grades[id]['user']['name'] = name
-    student_grades[id]['user']['password'] = m
-    file = open("gc_grades.json", "w")
-    file.write(json.dumps(student_grades))
-    file.close()
+def Teachers(teachers, student_grades, user, id, name, m):
+    if user == "Teacher" or user == "teacher":
+        if id in teachers.keys():
+            print "Here is the list of all students and their grades:"
+            for key in student_grades:
+                print "Student id:", key
+                print "Student name:", student_grades[key]['user']['name']
+                print "Grades:"
+                for i in student_grades[key]["grades"]:
+                    print i,"-",student_grades[key]["grades"][i]
+                print
 
-def printCurrentGrade(grades, current_grades, name, id):
+            teacher_input = raw_input("Want to change someone's grade? ")
+            if teacher_input == "Yes" or teacher_input == "yes":
+                teacher_input = raw_input("Input the id of student whose grades you want to change ")
+                assignment = raw_input("For which assignment you want to change the grade? Input end when finished ")
+                while assignment != "end":
+                    student_grades[teacher_input]["grades"][assignment] = int(raw_input("The current grade is " \
+                                                                  + str(student_grades[teacher_input]["grades"][assignment]) + ". Enter the new grade "))
+                    assignment = raw_input("For which assignment you want to change the grade? Input end when finished ")
+            else:
+                print "Have a nice day!"
+        else:
+            teachers[id] = {}
+            teachers[id]['user'] = {}
+            teachers[id]['user']['name'] = name
+            teachers[id]['user']['password'] = m
+            file = open("gc_teachers.json", "w")
+            file.write(json.dumps(teachers))
+            file.close()
+
+    return student_grades
+
+def saveGrades(student_grades, current_grades, name, id, m):
+    try:
+        student_grades[current_grades.keys()[0]] = current_grades[current_grades.keys()[0]]
+        student_grades[id]['user']['name'] = name
+        student_grades[id]['user']['password'] = m
+        file = open("gc_grades.json", "w")
+        file.write(json.dumps(student_grades))
+        file.close()
+    except:
+        file = open("gc_grades.json", "w")
+        file.write(json.dumps(student_grades))
+        file.close()
+
+def printCurrentGrade(grades, student_grades, id):
     curr_grade = 0
-    for key in current_grades[id]["grades"]:
-        if current_grades[id]["grades"][key] != -1:
-            calc_grade = float(current_grades[id]["grades"][key]) * grades[key] / 100
+    for key in student_grades[id]["grades"]:
+        if student_grades[id]["grades"][key] != -1:
+            calc_grade = float(student_grades[id]["grades"][key]) * grades[key] / 100
             curr_grade = curr_grade + calc_grade
 
     return curr_grade
@@ -119,10 +191,15 @@ def matrix(curr_grade, conv_matrix):
 def main():
     grades, conv_matrix = loadSetupData()
     student_grades = loadStudentGrades()
-    name, id, password = askForUserInfo(student_grades)
-    m = checkPassword(id, name, password, student_grades)
-    current_grades = askForAssignmentMarks(grades, student_grades, id)
+    teachers = loadTeachers()
+    user, name, id, password = askForUserInfo()
+    m = checkPassword(id, name, password, student_grades, teachers)
+    current_grades = askForAssignmentMarks(user, grades, student_grades, id)
+    student_grades = Teachers(teachers, student_grades, user, id, name, m)
     saveGrades(student_grades, current_grades, name, id, m)
-    curr_grade = printCurrentGrade(grades, current_grades, name, id)
-    matrix(curr_grade, conv_matrix)
+    try:
+        curr_grade = printCurrentGrade(grades, student_grades, id)
+        matrix(curr_grade, conv_matrix)
+    except:
+        pass
 main()
